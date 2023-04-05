1235b2fcb1cb236c292ba5787898f6afbb51e5ab
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-04-05 19:23:54 +0700

    upload folder and file section7-8

---
+++ section7-8/section7.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('#btn');
+
+const textReal = 'Hai nama saya Nabil';
+btn.textContent = textReal;
+
+function updateText() {
+     btn.textContent = 'Nabil Anugerah Pangestu';
+}
---
+++ section7-8/section8.js
@@ -0,0 +1,5 @@
+function myCar() {
+     let car = document.getElementById('car').value;
+     
+     alert(`mobil kamu adalah ${car}`);
+}
